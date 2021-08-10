5b3d95d31cf56d5d0e6fe636ad0a15dad0c3fd24
AmeN <pawel.reclaw@gmail.com>
2021-08-10 20:19:50 +0200

    Design patterns

--- src/main/java/com/sda/zdjavapol92/dp/Main.java
+++ src/main/java/com/sda/zdjavapol92/dp/Main.java
@@ -1,4 +1,25 @@
 package com.sda.zdjavapol92.dp;
 
 public class Main {
+    // Wzorce projektowe - eleganckie/praktyczne sposoby na rozwiązywanie problemów programistycznych
+    //                      life hack - na rozwiązywanie problemów
+    //
+    // SOLID - jeśli piszemy kod zgodny z SOLID, to jesteśmy super fajni
+    //
+    // Kategorie:
+    //  - stworzeniowe - za stworzenie obiektów
+    //                  - builder (problem wielu parametrów w konstruktorze)
+    //                  - singleton (problem pojedynczej instancji obiektu)
+    //                  - factory / abstract factory (problem częstego tworzenia obiektów pewnego rodzaju)
+    //                  -                            (problem ograniczenia możliwości stworzenia własnego obiektu)
+    //
+    //  - behawioralne - za zachowanie obiektów
+    //                  - strategy (problem zachowania obiektu - chcemy żeby możliwe było zmienianie działania obiektu
+    //                  -           bez konieczności jego edytowania. Zamiast tego zmieniamy obiekt wewnątrz (Setterem))
+    //
+    //  - strukturalne - za strukturę kodu
+    //                  - decorator (problem braku możliwości dziedziczenia, dodajemy nowe funkcjonalności bez istotnego
+    //                                  ingerowania w istniejącą implementację)
+    //                  - adapter   (problem realizowania podobnych funkcjonalności ale nie w jednakowy sposób, np. nazwa
+    //                                  metody może być inna)
 }
